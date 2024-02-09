a25a0f5c82e96e16cfc614a250bc3f70450b4d41
ran <rdavee0412@gmail.com>
2024-02-09 19:25:05 +0800

    Chakra UI changes, Better UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useEffect, useState } from 'react'
-import { MainApp } from './styled'
+import { useEffect, useState } from 'react';
+import { 
+        Heading,
+        Text,
+        Button,
+        Container,
+        Center,
+        Mark } from '@chakra-ui/react';
 
-function App() {
+export const App = () => {
 
     const storageKeyName = "count";
 
@@ -16,12 +22,30 @@ function App() {
     }, [count])
 
 return (
-    <MainApp>
-        Click Me
-        <button onClick={() => addNumber(count)}>
-            count is {count}
-        </button>
-    </MainApp>
+    // react fragment
+    <>
+        <Container maxW = {['full', 'container.lg']} mt={6}>
+            <Center>
+                <Heading fontSize='30px' fontFamily='NewYork'>Welcome! </Heading>
+            </Center>
+            <Center>
+                <Text fontSize='15px' fontFamily='NewYork' mb={5}>This is my first react app </Text>
+            </Center>
+            <Center>
+                <Mark bg='lightgrey' color='black' m={4} px={2} py={1} borderRadius={3}>Count is already {count}!</Mark>
+            </Center>
+            <Center>    
+                <Button 
+                height='48px'
+                width='200px'
+                border='2px'
+                variant='solid' 
+                fontStyle='italic'
+                fontSize='15px'
+                onClick={() => addNumber(count)}>Count me!</Button>
+            </Center>
+        </Container>
+    </>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { App } from './App';
+
+const theme = extendTheme({
+    colors: {
+        brand: {
+            100: "#f7fafc",
+            // ...
+            900: "#1a202c",
+        },
+    },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
+</React.StrictMode>,
 )
